37e4db51f5d5d3e43a0208a602a2812cccac3313
Ignacio Jonas <ignacio.jonas@eclinicalos.com>
2016-04-08 11:34:24 -0300

    Create App component

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'sm-app',
+    templateUrl: 'app.component.html',
+})
+
+export class AppComponent{
+  pageTitle: string = 'UNICEN Students Management System';
+}
